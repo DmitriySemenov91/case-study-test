36850ca13495fe9c7fc3efdbcfdc60b33cd8207c
Dima Semenov <dsemenov@asist.pl>
2022-04-15 12:39:07 +0200

    fix: radio part

--- src/App/App.js
+++ src/App/App.js
@@ -9,44 +9,12 @@ import "./App.css";
 
 import { useSpring, animated } from "react-spring";
 
-function MinusCircle(props) {
-  return (
-    <SvgIcon
-      fontSize="inherit"
-      style={{
-        width: 14,
-        height: 14,
-        borderRadius: 10,
-        background: "blueviolet",
-      }}
-      {...props}
-    >
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
-    </SvgIcon>
-  );
-}
-function PlusCircle(props) {
-  return (
-    <SvgIcon
-      fontSize="inherit"
-      style={{
-        width: 14,
-        height: 14,
-        borderRadius: 10,
-        background: "blueviolet",
-      }}
-      {...props}
-    >
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
-    </SvgIcon>
-  );
-}
-
 function MinusSquare(props) {
   return (
     <SvgIcon
+      id="radio"
       fontSize="inherit"
-      style={{ width: 14, height: 14, borderRadius: 2 }}
+      style={{ width: 14, height: 14 }}
       {...props}
     >
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
@@ -56,8 +24,9 @@ function MinusSquare(props) {
 function PlusSquare(props) {
   return (
     <SvgIcon
+      id="radio"
       fontSize="inherit"
-      style={{ width: 14, height: 14, borderRadius: 2 }}
+      style={{ width: 14, height: 14 }}
       {...props}
     >
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
@@ -107,9 +76,6 @@ const StyledTreeItem = styled((props) => (
     paddingLeft: 18,
     borderLeft: `1px solid ${alpha(theme.palette.text.primary, 0.4)}`,
   },
-  labelRoot: {
-    borderRadius: 2,
-  },
 }));
 
 function CustomizedTreeView() {
@@ -132,12 +98,12 @@ function CustomizedTreeView() {
             key={user.name + user.username + `${user.id}`}
             nodeId={`${user.id}2`}
             label={user.name}
+            id="container"
           >
             <StyledTreeItem
               nodeId="3"
               label={`email: ${user.email}`}
               className="changeColor"
-              radio
             />
             <StyledTreeItem nodeId="4" label={`phone: ${user.phone}`} />
             <StyledTreeItem
@@ -145,7 +111,7 @@ function CustomizedTreeView() {
               label={`website: ${user.website}`}
               className="changeColor"
             />
-            <StyledTreeItem nodeId={`${user.id}6`} label="company">
+            <StyledTreeItem nodeId={`${user.id}6`} label="company" id="child">
               <StyledTreeItem
                 nodeId="6"
                 label={`Name: ${user.company.name}`}
@@ -161,7 +127,7 @@ function CustomizedTreeView() {
                 className="changeColor"
               />
             </StyledTreeItem>
-            <StyledTreeItem nodeId={`${user.id}0`} label="address">
+            <StyledTreeItem nodeId={`${user.id}0`} label="address" id="child">
               <StyledTreeItem
                 nodeId="11"
                 label={`street: ${user.address.street}`}
